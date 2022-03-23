f6a1e73d2a3325bcfd5746333bdc42cff2db7aaf
Pablo Najera <pablo@najera.cc>
2022-03-23 13:15:19 -0400

    checking to see if EB has valid EB functionality

--- js/second-screen.js
+++ js/second-screen.js
@@ -8,11 +8,14 @@ $secondScreen = (function(){
 ///BARCODE FUNCTIONALITY
 
     function enableScan() {
-        EB.Barcode.enable({
-            allDecoders: true,
-            autoTab: true,
-            lowBatteryScan: true,
-        });
+        if (EB && EB.Barcode && EB.Barcode.enable) {
+            $('#title').text('Success!')
+            EB.Barcode.enable({
+                allDecoders: true,
+                autoTab: true,
+                lowBatteryScan: true,
+            });
+        }
     }
     function barcodeScanned(barcodeNumber) {
         $(elementSelector.scanData).text("barcode: " + barcodeNumber.data);
