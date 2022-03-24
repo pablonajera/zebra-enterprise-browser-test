18ef19acdb8dcfb624b6b19e9b11b0a48370fb86
Pablo Najera <pablo@najera.cc>
2022-03-24 15:03:33 -0400

    trying another method

--- js/second-screen.js
+++ js/second-screen.js
@@ -25,36 +25,52 @@ $secondScreen = (function(){
 ///CAMERA FUNCTIONALITY
 
 
-function picture_taken_callback(params) {
-  // Did we receive an image?
-  if (params["status"]=="ok") {
-    // show it in our <img id="captured_image"< tag
-    $("#image").attr("src", EB.Application.expandDatabaseBlobFilePath(params["imageUri"]));
-  }
-
+function picture_taken_callback(cbData) {
+    // Did we receive an image?
+    if ((cbData.imageUri != undefined) || (cbData.imageUri != null)){
+        document.getElementById('image').style.height = "250px";
+        document.getElementById('image').style.width = "250px";
+        document.getElementById('image').src = 'http://localhost:' + EB.System.localServerPort + cbData.imageUri;
+    }
+    else{
+        document.getElementById('image').style.height = "";
+        document.getElementById('image').style.width = "";
+        document.getElementById('image').src = "";
+    }
 }
 
     function enableImageCapture() {
-        console.log('Camera opening...');
-        EB.Camera.outputForma = "OUTPUT_FORMAT_IMAGE_PATH";
-        EB.Camera.takePicture({}, function(imageInfo) {
-            console.log('Camera closing...');
-            setTimeout(() => {
-                if (imageInfo) {
-                    console.log('imageinfo is valid -> ');
-                    console.log(imageInfo);
-                    console.log(imageInfo.status);
-                    console.log(imageInfo.imageUri);
-                    if (imageInfo["status"]=="ok" || imageInfo.status == 'ok') {
-                        // Assuming we have an  tag, we will be able to see the image that was just captured
-                        $(elementSelector.image).attr("src", imageInfo["imageUri"]);
-                    }
-                }
-                else {
-                    console.log('image info is not valid');
-                }
-            }, 4000);
-        });
+
+
+function takePicture(){
+    var param = {
+        'outputFormat':'image',
+    };
+    EB.Camera.takePicture(param, picture_taken_callback);
+
+
+
+
+        // console.log('Camera opening...');
+        // EB.Camera.outputFormat = "OUTPUT_FORMAT_IMAGE_PATH";
+        // EB.Camera.takePicture({}, function(imageInfo) {
+        //     console.log('Camera closing...');
+        //     setTimeout(() => {
+        //         if (imageInfo) {
+        //             console.log('imageinfo is valid -> ');
+        //             console.log(imageInfo);
+        //             console.log(imageInfo.status);
+        //             console.log(imageInfo.imageUri);
+        //             if (imageInfo["status"]=="ok" || imageInfo.status == 'ok') {
+        //                 // Assuming we have an  tag, we will be able to see the image that was just captured
+        //                 $(elementSelector.image).attr("src", imageInfo["imageUri"]);
+        //             }
+        //         }
+        //         else {
+        //             console.log('image info is not valid');
+        //         }
+        //     }, 4000);
+        // });
     }
 
     return {
