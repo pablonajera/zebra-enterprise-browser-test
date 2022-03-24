130d0f603ecdd699ca80574f7765969c6febbd51
Pablo Najera <pablo@najera.cc>
2022-03-24 16:49:14 -0400

    camera attempt 29381

--- js/second-screen.js
+++ js/second-screen.js
@@ -25,53 +25,19 @@ $secondScreen = (function(){
 ///CAMERA FUNCTIONALITY
 
 
-function picture_taken_callback(cbData) {
-    // Did we receive an image?
-    if ((cbData.imageUri != undefined) || (cbData.imageUri != null)){
-        document.getElementById('image').style.height = "250px";
-        document.getElementById('image').style.width = "250px";
-        document.getElementById('image').src = 'http://localhost:' + EB.System.localServerPort + cbData.imageUri;
-    }
-    else{
-        document.getElementById('image').style.height = "";
-        document.getElementById('image').style.width = "";
-        document.getElementById('image').src = "";
-    }
-}
-
-
-
-function enableImageCapture(){
-
-    const enumData = EB.Camera.enumerate();
-    var param = {
-        'outputFormat':'image',
-    };
-    enumData[0].takePicture(param, picture_taken_callback);
-
-
-
-
-        // console.log('Camera opening...');
+    function enableImageCapture() {
         // EB.Camera.outputFormat = "OUTPUT_FORMAT_IMAGE_PATH";
-        // EB.Camera.takePicture({}, function(imageInfo) {
-        //     console.log('Camera closing...');
-        //     setTimeout(() => {
-        //         if (imageInfo) {
-        //             console.log('imageinfo is valid -> ');
-        //             console.log(imageInfo);
-        //             console.log(imageInfo.status);
-        //             console.log(imageInfo.imageUri);
-        //             if (imageInfo["status"]=="ok" || imageInfo.status == 'ok') {
-        //                 // Assuming we have an  tag, we will be able to see the image that was just captured
-        //                 $(elementSelector.image).attr("src", imageInfo["imageUri"]);
-        //             }
-        //         }
-        //         else {
-        //             console.log('image info is not valid');
-        //         }
-        //     }, 4000);
-        // });
+        EB.Camera.takePicture({}, function(imageInfo) {
+            if (imageInfo) {
+                if (imageInfo["status"]=="ok") {
+                    // Assuming we have an  tag, we will be able to see the image that was just captured
+                    $(elementSelector.image).attr("src", imageInfo["imageUri"]);
+                }
+                else if (imageInfo.status == 'ok') {
+                    $(elementSelector.image).attr("src", imageInfo.imageUri);
+                }
+            }
+        });
     }
 
     return {
