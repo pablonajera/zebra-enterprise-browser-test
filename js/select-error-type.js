0abb52fa1f3558eae6e7b947f31c3c839731f748
DESKTOP-4Q1O459\katmo <molkat17@icloud.com>
2022-04-14 15:42:39 -0400

    Added camera capture to selecterrortype page

--- js/select-error-type.js
+++ js/select-error-type.js
@@ -82,10 +82,28 @@ function showErrorForm7() {
         }
         });
 }
+///CAMERA FUNCTIONALITY
+function enableImageCapture() {
+        console.log('Opening camera!');
+        EB.Camera.takePicture({}, function(imageInfo) {
+            console.log('IMAGE TAKEN');
+            if (imageInfo) {
+                console.log(imageInfo);
+                if (imageInfo.status == 'ok') {
+                    console.log(imageInfo.imageUri);
+                    $(elementSelector.images).show();
+                    const imageLocation = 'http://localhost:' + EB.System.localServerPort + imageInfo.imageUri;
+                    const newImage = '<img class="ui medium image" src="' + imageLocation + '">';
+                    $(elementSelector.images).append(newImage);
+                }
+            }
+        });
+    }
 
 
 
     return {
+        enableImageCapture: enableImageCapture,
         initialize: function(){
             showErrorForm1();
             showErrorForm2();
